005e44292379a0df8cfeaccaa2e83578f1130e78
nils måsén <nils@piksel.se>
2015-12-30 00:59:54 +0100

    upgraded uptime module to comply with API 1.1

--- Hale-CheckPack-Core/Hale-Check-Uptime/Hale-Check-Uptime.cs
+++ Hale-CheckPack-Core/Hale-Check-Uptime/Hale-Check-Uptime.cs
@@ -1,69 +1,37 @@
-﻿using Hale_Lib.Checks;
+﻿using HaleLib.Modules;
+using HaleLib.Modules.Info;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-using static Hale_Lib.Utilities.TimeSpanFormatter;
+using static HaleLib.Utilities.TimeSpanFormatter;
 
 namespace Hale.Checks
 {
-    /// <summary>
-    /// This is a mandatory class that should contain all information regarding the check. This will be instantiated and added to the dynamic list in the Agent.
-    /// </summary>
-    public class UptimeCheck: ICheck
+    // Todo: Change this module to Power perhaps? -NM 2015-12-30
+
+    public class UptimeCheck: Module, IInfoProvider
     {
 
-        /// <summary>
-        /// The name of the check. This will be visible in the Web UI.
-        /// For example: "System Uptime"
-        /// </summary>
-        public string Name { get; } = "System Uptime";
+        public new string Name { get; } = "System Uptime";
 
-        /// <summary>
-        /// Person and organization (opt) that developed the check.
-        /// </summary>
-        public string Author { get; } = "hale project";
+        public new string Author { get; } = "hale project";
 
-        /// <summary>
-        /// Internal version of the check itself.
-        /// </summary>
-        public Version Version { get; } = new Version(0, 1, 1);
+        public override string Identifier { get; } = "com.itshale.core.uptime";
 
-        /// <summary>
-        /// What platform is this check targeted at?
-        /// Might be a specific release of Windows, Linux, OS/400 etc.
-        /// </summary>
-        public string Platform { get; } = "Windows";
+        public new Version Version { get; } = new Version(0, 1, 1);
 
-        /// <summary>
-        /// </summary>
-        public decimal TargetApi { get; } = 0.1M;
+        public override string Platform { get; } = "Windows";
 
-        /// <summary>
-        /// </summary>
-        public bool ParallelExecution { get; } = true;
+        public decimal TargetApi { get; } = 1.1M;
 
-        /// <summary>
-        /// Set all the attributes above directly in the constructor.
-        /// </summary>
-        public UptimeCheck()
+        Dictionary<string, ModuleFunction> IModuleProviderBase.Functions { get; set; }
+            = new Dictionary<string, ModuleFunction>();
+
+        public InfoResult DefaultInfo(InfoTargetSettings settings)
         {
-        }
-
-
-        /// <summary>
-        /// Executes the check and returns a Response instance. This is then serialized in
-        /// the agent to JSON. This method must always be named Execute, and should always return a
-        /// response following the stated standard. However, you are free to add as much logic as your
-        /// use case requires in other classes/methods.
-        /// 
-        /// Any checks not adhering to this standard will not be merged into the checkpacks.
-        /// </summary>
-        /// <param name="settings"></param>
-        public CheckResult Execute(CheckTargetSettings settings)
-        {
-            CheckResult result = new CheckResult();
+            var result = new InfoResult(settings.Target);
 
             try {
                 TimeSpan uptime = new TimeSpan();
@@ -77,26 +45,25 @@ namespace Hale.Checks
                     uptime = TimeSpan.FromSeconds(_raw);
                 }
 
-                result.RawValues.Add(new DataPoint("uptimeSeconds", _raw));
+                result.Items.Add("uptimeSeconds", _raw.ToString());
 
                 result.Message = "Uptime: " + HumanizeTimeSpan(uptime);
-
-                result.SetThresholds(_raw, settings.Thresholds);
 
                 result.RanSuccessfully = true;
             }
             catch (Exception x)
             {
-                result.CheckException = x;
+                result.ExecutionException = x;
                 result.RanSuccessfully = false;
             }
 
             return result;
         }
 
-        public void Initialize(CheckSettings settings)
+        public void InitializeInfoProvider(InfoSettings settings)
         {
-
+            this.AddInfoFunction(DefaultInfo);
+            this.AddInfoFunction("uptime", DefaultInfo);
         }
 
     }
