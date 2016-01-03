bc35d948549bac7aac665499717afc954933e16f
nils måsén <nils@piksel.se>
2016-01-03 18:36:47 +0100

    reverted framework version to 4.5 and updated hale-lib references

--- Hale-CheckPack-Core/Hale-Check-Memory-Usage/Properties/AssemblyInfo.cs
+++ Hale-CheckPack-Core/Hale-Check-Memory-Usage/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.16001.1")]
-[assembly: AssemblyFileVersion("1.2.16001.1")]
+[assembly: AssemblyVersion("1.2.16003.4")]
+[assembly: AssemblyFileVersion("1.2.16003.4")]
